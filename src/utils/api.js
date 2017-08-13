0b73412f9f21b945949fcedf6cbe5618d6065191
kissmygritts <kissmygritts08@gmail.com>
2017-08-13 14:19:13 -0700

    api communications updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const api = axios.create({
-  // baseURL: 'http://localhost:1313'
-  baseURL: 'http://34.208.55.29:1313'
+  baseURL: 'http://localhost:1313'
+  // baseURL: 'http://34.208.55.29:1313'
 })
 
 export function getSpeciesList () {
